195899d294b4ca4954e89a44fa05bea77d276bd9
GaboGomezT <gomezgabriel09@gmail.com>
2021-06-07 00:04:03 -0500

    made basic app with form

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-import fastapi
-import uvicorn
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
 
-app = fastapi.FastAPI()
-
-
-@app.get('/')
-def index():
-    return {
-        'message': "Hello world"
-    }
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates/")
 
 
-if __name__ == '__main__':
-    uvicorn.run(app)
+@app.get("/")
+def form_post(request: Request):
+    result = "Type a number"
+    return templates.TemplateResponse(
+        "index.html", context={"request": request, "result": result}
+    )
+
+
+@app.post("/")
+def form_post(
+    request: Request,
+    num: int = Form(...),
+    fname: str = Form(...),
+    diet: str = Form(...),
+):
+    result = [num, fname, diet]
+    return templates.TemplateResponse(
+        "index.html", context={"request": request, "result": result}
+    )
