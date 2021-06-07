84bb0251b26758af84f074feff56b929d8c47f24
GaboGomezT <gomezgabriel09@gmail.com>
2021-06-07 18:10:35 -0500

    added country select

--- main.py
+++ main.py
@@ -6,23 +6,35 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates/")
 
+lived_in_options = ["USA", "United Kingdom", "China", "Mexico"]
+
 
 @app.get("/")
 def form_post(request: Request):
     result = "Type a number"
     return templates.TemplateResponse(
-        "index.html", context={"request": request, "result": result}
+        "index.html",
+        context={
+            "request": request,
+            "result": result,
+            "countries": lived_in_options,
+        },
     )
 
 
 @app.post("/")
 def form_post(
     request: Request,
-    num: int = Form(...),
+    num: float = Form(...),
     fname: str = Form(...),
     diet: str = Form(...),
 ):
     result = [num, fname, diet]
     return templates.TemplateResponse(
-        "index.html", context={"request": request, "result": result}
+        "index.html",
+        context={
+            "request": request,
+            "result": result,
+            "countries": lived_in_options,
+        },
     )
